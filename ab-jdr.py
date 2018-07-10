d2f1ef7d9a8f5f213ca452f0fc078c1e6a4876da
jdrgithub <jdoggus@gmail.com>
2018-07-10 02:41:43 -0400

    Added list of functions to main.

--- ab-jdr.py
+++ ab-jdr.py
@@ -5,5 +5,35 @@ def main():
 	""" Main entry point for the script."""
 	pass
 
-if __name__ == '__main__':
+
+def check_4_file(contacts_file):
+	""" Check for contacts.txt file """
+	pass
+
+def add_contact(name, address, phone, email):
+    """ Add contact """
+	pass
+
+def print_book():
+	""" Print book """
+	pass
+
+def contact_search(name):
+	""" Contact search """
+	pass
+
+def modify_contact(name):
+	""" Modify contact """
+	pass
+
+def delete_contact(name):
+	""" Delete contact """
+	pass
+
+def manage_menu():
+	""" Manage menu """
+	pass
+
+
+ __name__ == '__main__':
 	sys.exit(main())

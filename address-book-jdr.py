764a95a6ef865e530481bbbe1d81e762b60096f9
jdrgithub <jdoggus@gmail.com>
2018-07-12 00:34:55 -0400

    Moved while loop after function declarations.

--- address-book-jdr.py
+++ address-book-jdr.py
@@ -9,38 +9,13 @@ def menu():
     """ Main Menu """
     print(30 * "-", "MENU", 30 * "-")
     print("1. Add a record")
-    print("2. Delete a record") 
+    print("2. Delete contact") 
     print("3. Look up record")
     print("4. Change a record")
     print("5. Display all records")
     print("6. Check contact file")
     print("7. Exit")
     print(67 * "-")
-		
-loop=True
-while loop:
-    menu()
-    choice = input("What would you like to do? ")
-
-    if choice == 1:
-        print("\n Add Record")
-        add_contact()
-    elif choice == 2:
-        print("\n Delete Record")
-    elif choice == 3:
-        print("\n Find Record")
-    elif choice == 4:
-        print("\n Change Record")
-    elif choice == 5:
-        print("\n Display All Records")
-        print_book()
-    elif choice == 6:
-        print("\n Check on contact.txt")
-        check()
-    elif choice == 7:
-        sys.exit("Goodbye")			
-    else: 
-        print("\n Try again") 
 
 def check():
     """ Check for contacts.txt file """
@@ -63,26 +38,25 @@ def add_contact():
         line = str(':'.join(list))
         file_object.write(str(line) + "\n")
 		
-def delete_contact(name):
+def delete_contact():
     """ Delete contact """
-	""" General steps
-		Ask the user for a name to delete record of
-		Read in all records to a list, unless the first field matches the supplied name
-		Write lists out to contacts.txt  """
-	filename = 'contacts.txt'
-	name = raw_input("Name to delete record of? ")
-	with open(filename) as file_object:
-	    lines = f_obj.readlines()
+    """ General steps
+	Ask the user for a name to delete record of
+	Read in all records to a list, unless the first field matches the supplied name
+	Write lists out to contacts.txt  """
+    filename = 'contacts.txt'
+    name = raw_input("Name to delete record of? ")
+    with open(filename) as file_object:
+        lines = f_obj.readlines()
     list_out = []		
-	for line in lines:
-	    if line[0] == name:
+    for line in lines:
+        if line[0] == name:
             continue
-		else:
+        else:
             list_out.append(line)		
     with open(filename, 'w') as file_object:
         file_object.write(str(list_out))	
 		    
-
 def print_book():
     """ Display contents of contacts.txt file."""
     filename = 'contacts.txt'
@@ -112,17 +86,45 @@ def read_file():
         print(line.rstrip())
 
 		
-def contact_search(name):
+def find_contact(name):
     """ Contact search """
     pass
 
-def modify_contact(name):
+def modify_contact_data(name):
     """ Modify contact """
     """ Do a conditional test """
     """ like name in list """
     pass
 
 
+		
+loop=True
+while loop:
+    menu()
+    choice = input("What would you like to do? ")
+
+    if choice == 1:
+        print("\n Add Record")
+        add_contact()
+    elif choice == 2:
+        print("\n Delete contact")
+        delete_contact()
+    elif choice == 3:
+        print("\n Find Record")
+        find_record()
+    elif choice == 4:
+        print("\n Change Record")
+        modify_contact_data()
+    elif choice == 5:
+        print("\n Display All Records")
+        print_book()
+    elif choice == 6:
+        print("\n Check on contact.txt")
+        check()
+    elif choice == 7:
+        sys.exit("Goodbye")			
+    else: 
+        print("\n Try again") 
 
 
 if __name__ == '__main__':
